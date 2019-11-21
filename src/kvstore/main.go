45234bec4328a698d443ea828adfb07639403078
Jay Guo <guojiannan1101@gmail.com>
2019-11-21 00:54:29 -0800

    Fix evil tx
    Signed-off-by: Jay Guo <guojiannan1101@gmail.com>

--- src/kvstore/main.go
+++ src/kvstore/main.go
@@ -113,6 +113,11 @@ func (t *SimpleChaincode) invoke(stub shim.ChaincodeStubInterface, args []string
 	if err != nil {
 		return shim.Error("Invalid transaction amount, expecting a integer value")
 	}
+
+	if X <= 0 || Aval < X {
+		return shim.Error("Evil transaction")
+	}
+
 	Aval = Aval - X
 	Bval = Bval + X
 	log.Printf("Aval = %d, Bval = %d\n", Aval, Bval)
